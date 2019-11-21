7dac8f6f826696f811b40f4ed1b844cf9bd4cab4
Louis Chan <louis@bottle.mo>
2019-11-21 18:24:27 +0800

    Remove styled components from storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,3 @@
-import React from 'react'
-import styled from 'styled-components'
-import { addDecorator, configure } from '@storybook/react'
-
-
-const Root = styled.div`
-  margin: 0;
-  width: 100vw;
-  height: 100vh;
-`
-
-addDecorator(storyFn => <Root>{storyFn()}</Root>)
+import { configure } from '@storybook/react'
 
 configure(require.context('../src', true, /\.stories\.js$/), module)
